9105ed62e109141fe942cbc023cb286e045c8c56
Erasto73 <erastochina@gmail.com>
2023-08-31 12:10:58 +0300

    first commit

---
+++ app/data.js
@@ -0,0 +1,36 @@
+export const quiz = {
+    totalQuestions: 5,
+    questions: [
+      {
+        id: 1,
+        question: 'What is the capital of Tanzania?',
+        answers: ['Madrid', 'Dar es Salaam', 'Rome', 'Berlin'],
+        correctAnswer: 'Dar es Salaam',
+      },
+      {
+        id: 2,
+        question: 'What is the largest planet in our solar system?',
+        answers: ['Mars', 'Jupiter', 'Venus', 'Saturn'],
+        correctAnswer: 'Jupiter',
+      },
+      {
+        id: 3,
+        question: 'What is the name of iceland in Tanzania ?',
+        answers: ['Monaco', 'Maldives', 'Zanzibar', 'San Marino'],
+        correctAnswer: 'Zanzibar',
+      },
+      {
+        id: 4,
+        question: 'What is the national spoken language in Tanzania?',
+        answers: ['English', 'Kiswahili', 'Spanish', 'Hindi'],
+        correctAnswer: 'Kiswahili',
+      },
+      {
+        id: 5,
+        question: 'Who is the founder of Microsoft?',
+        answers: ['Steve Jobs', 'Bill Gates', 'Elon Musk', 'Mark Zuckerberg'],
+        correctAnswer: 'Bill Gates',
+      },
+    ],
+  };
+  
